63b9859ad4beb4588626a3a3601ec5f48c01de2c
Kevin McFarland <miamimc305@bellsouth.net>
2020-04-04 13:22:39 -0500

    got the axes set up properly

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 // define the height and width of the chart
-const h = 768;
-const w = 1024;
-const padding = 50;
+const h = window.innerHeight;
+const w = window.innerWidth;
+const margin = ({top: 20, right: 50, bottom: 20, left: 50});
 
 // get the data
 let req = new XMLHttpRequest();
 req.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", false);
 req.send();
-const workingData = JSON.parse(req.response).data;
-const yValues = workingData.map(d => d[1])
+const jso = JSON.parse(req.response);
+
+// get the starting and ending date
+const minDate = new Date(jso.from_date);
+const maxDate = new Date(jso.to_date);
+
+console.log(minDate);
+console.log(maxDate);
 
 // calculate bar width
-const barWidth = ((w - padding) / workingData.length);
+const barWidth = d3.scaleBand()
+    .domain(d3.range(jso.data.length))
+    .range([margin.left, w - margin.right])
+    .bandwidth()
 
 // create the scale factor on the y axis
 const yScale = d3.scaleLinear()
-    .domain([d3.min(yValues), d3.max(yValues)])
-    .range([0, (h - padding)]);
+    .domain([0, d3.max(jso.data.map(d => d[1]))])
+    .range([h - margin.bottom, margin.top]);
+
+// create the scale factor on the x axis
+const xScale = d3.scaleTime()
+    .domain([minDate, maxDate])
+    .range([0, w - margin.left - margin.right])
 
 // create the svg with the set height/width
-d3.select("body").append("svg")
+const svg = d3.select("body").append("svg")
     .attr("height", h)
     .attr("width", w);
 
+// create the y axis
+const yAxis = d3.axisLeft(yScale);
+
+svg.append("g")
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + margin.left + ", 0)")
+    .call(yAxis);
+
+// create the x axis
+const xAxis = d3.axisBottom(xScale)
+    .ticks(d3.timeYear.every(5));
+
+svg.append("g")
+    .attr("id", "x-axis")
+    .call(xAxis)
+    .attr("transform", "translate(" + margin.left + ", " + (h - margin.bottom) +")");
+
 // visualize the data
 d3.select("svg").selectAll("rect")
-    .data(workingData)
+    .data(jso.data)
     .enter()
     .append("rect")
     .attr("class", "bar")
     .property("data-date", (d) => d[0])
     .property("data-gdp", (d) => d[1])
-    .attr("height", (d) => yScale(d[1]))
+    .attr("height", (d) => h - yScale(d[1]) - margin.top)
     .attr("width", barWidth)
-    .attr("x", (d, i) => (padding + (barWidth * i)))
-    .attr("y", (d) => (h - padding - yScale(d[1])))
+    .attr("x", (d, i) => (margin.left + (barWidth * i)))
+    .attr("y", (d) => yScale(d[1]))
     .attr("fill", "black");
